58819bd37f160ec3abef07d1bd29c871e9a7f222
Ryan Bryce <ryan.bryce1992@gmail.com>
2017-12-04 23:05:45 -0700

    got basic layout done brought in inquirer

---
+++ Homework/game.js
@@ -0,0 +1,22 @@
+var inquirer = require('inquirer');
+//this will be for our game logic
+//recieveng user input and ansking questions via inquirer
+    function askForLetter (){
+        var inquirer = require('inquirer');
+        inquirer.prompt([{
+            name: letter,
+            type: "input",
+            message: "Please type in a letter to guess."
+    
+        }]).then(answers => {
+            
+        });
+    }
+
+
+//computer choses random word
+    //passes word to word.js
+
+
+
+//user is prompt to type letter
---
+++ Homework/word.js
@@ -0,0 +1,8 @@
+
+console.log();
+
+//Used to create an object representing the current word the user is attempting to guess.This should contain word specific logic and data.
+function Word(word) {
+
+};
+module.exports = Word;
